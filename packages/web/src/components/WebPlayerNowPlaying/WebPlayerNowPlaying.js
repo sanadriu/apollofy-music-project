75db287ca7159c47df36346b7e5c45cfd01365f8
Jcarri14 <j.carrion@students.salle.url.edu>
2021-03-29 12:29:57 +0200

    feat: add WebPlayerNowPlaying component

---
+++ packages/web/src/components/WebPlayerNowPlaying/WebPlayerNowPlaying.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./WebPlayerNowPlaying.scss";
+
+function WebPlayerNowPlaying() {
+  return (
+    <footer className="WebPlayer__now-playing" aria-label="player controls">
+      now playing
+    </footer>
+  );
+}
+
+export default WebPlayerNowPlaying;
