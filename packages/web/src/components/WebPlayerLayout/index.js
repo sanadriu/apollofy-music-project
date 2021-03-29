012000d3adb6c6714a712c0f7cb66233b26dbf82
Jcarri14 <j.carrion@students.salle.url.edu>
2021-03-29 12:29:12 +0200

    feat: add WebPlayerLayout component

---
+++ packages/web/src/components/WebPlayerLayout/index.js
@@ -0,0 +1,3 @@
+export { default as WebPlayerFooter } from "./WebPlayerFooter";
+export { default as WebPlayerLayout } from "./WebPlayerLayout";
+export { default as WebPlayerMainContainer } from "./WebPlayerMainContainer";
