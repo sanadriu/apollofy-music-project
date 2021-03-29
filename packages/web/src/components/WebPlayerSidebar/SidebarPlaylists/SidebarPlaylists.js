71e97df2e126a1910538442be9194cfbe07124f3
Jcarri14 <j.carrion@students.salle.url.edu>
2021-03-29 12:42:42 +0200

    feat: add SidebarPlaylists component

---
+++ packages/web/src/components/WebPlayerSidebar/SidebarPlaylists/SidebarPlaylists.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./SidebarPlaylists.scss";
+
+import PlaylistsHeader from "./PlaylistsHeader";
+
+function SidebarPlaylists() {
+  return (
+    <section className="SidebarPlaylists">
+      <PlaylistsHeader>playlists</PlaylistsHeader>
+    </section>
+  );
+}
+
+export default SidebarPlaylists;
