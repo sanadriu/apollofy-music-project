feb6ddafc6064a17da5d10a00218eca44f760413
Jcarri14 <j.carrion@students.salle.url.edu>
2021-03-29 12:05:46 +0200

    feat: add Button component

---
+++ packages/web/src/components/Button/Button.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { string, node, func, arrayOf, oneOf } from "prop-types";
+import cn from "clsx";
+
+import "./Button.scss";
+
+function Button({ type, variant, handleClick, classes, children, ...props }) {
+  const classNames = cn(
+    {
+      [`border-radius`]: true,
+      [`btn`]: true,
+      [`btn-${variant}`]: true,
+    },
+    ...classes,
+  );
+
+  return (
+    <button
+      className={classNames}
+      /** @see https://github.com/yannickcr/eslint-plugin-react/issues/1846#issuecomment-614921516 */
+      /* eslint-disable-next-line react/button-has-type */
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  handleClick: func,
+  children: node.isRequired,
+  classes: arrayOf(string),
+  type: oneOf(["button", "submit"]),
+  variant: oneOf(["primary", "secondary"]),
+};
+
+Button.defaultProps = {
+  handleClick: null,
+  classes: [],
+  type: "submit",
+  variant: "primary",
+};
+
+export default Button;
