5e38f6f28e100960b14b9f3b4f90dd442c834c06
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-12 12:50:46 +0200

    feat: add reducers to root-reducer

--- packages/web/src/redux/root-reducer.js
+++ packages/web/src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./auth/auth-reducer";
+import UserReducer from "./user/user-reducer";
+import PlaylistReducer from "./playlist/playlist-reducer";
+import SearchReducer from "./search/search-reducer";
+import TrackReducer from "./track/track-reducer";
+import SessionReducer from "./session/session-reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  users: UserReducer,
+  tracks: TrackReducer,
+  playlists: PlaylistReducer,
+  search: SearchReducer,
+  session: SessionReducer,
 });
 
 export default rootReducer;
