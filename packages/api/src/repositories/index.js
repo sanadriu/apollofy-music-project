f6b7fb1a901cc6e5d97bdebc8b0d4465e3a9562d
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-15 13:14:15 +0200

    feat: add new repos to index.js

--- packages/api/src/repositories/index.js
+++ packages/api/src/repositories/index.js
@@ -1,5 +1,13 @@
 const UserRepository = require("./user-repository");
+const TrackRepository = require("./track-repository");
+const PlaylistRepository = require("./playlist-repository");
+const PlaybackRepository = require("./playback-repository");
+const GenreRepository = require("./genre-repository");
 
 module.exports = {
   UserRepo: UserRepository,
+  TrackRepo: TrackRepository,
+  PlaylistRepo: PlaylistRepository,
+  PlaybackRepo: PlaybackRepository,
+  GenreRepo: GenreRepository,
 };
