87a998644691b64acc1bf952e26dd13217decd85
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-22 11:33:33 +0200

    feat: add genre router reference in indexjs

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { userRouter } = require("./user-routes");
 const { trackRouter } = require("./track-routes");
 const { playlistRouter } = require("./playlist-route");
+const { genreRouter } = require("./genre-routes");
 
 module.exports = {
   userRouter: userRouter,
   trackRouter: trackRouter,
   playlistRouter: playlistRouter,
+  genreRouter: genreRouter,
 };
