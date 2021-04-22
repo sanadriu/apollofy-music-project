8b54634e6169060cbc7e515263c1c3134bc16363
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-22 11:34:50 +0200

    feat: add genre routes

---
+++ packages/api/src/routes/genre-routes.js
@@ -0,0 +1,19 @@
+const Router = require("express").Router;
+
+const { authMiddleware } = require("../middlewares");
+const { genreController } = require("../controllers");
+
+const genreRouter = Router();
+
+genreRouter.post("/genres", authMiddleware, genreController.createGenre);
+genreRouter.get("/genres", authMiddleware, genreController.fetchGenres);
+//genreRouter.get("/genres/:id", authMiddleware, genreController.fetchGenreById);
+genreRouter.get(
+  "/genres/:name",
+  authMiddleware,
+  genreController.fetchGenreByName,
+);
+
+module.exports = {
+  genreRouter: genreRouter,
+};
