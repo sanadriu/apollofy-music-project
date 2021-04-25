50d56c59773c462ad70f658e5be630ffa5e5f4ec
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-26 01:56:20 +0200

    feat: create indexjs for pages import optimization

---
+++ packages/web/src/pages/index.js
@@ -0,0 +1,23 @@
+import Home from "./Home";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import ResetPassword from "./ResetPassword";
+import UploadSong from "./UploadSong";
+import UploadImage from "./UploadImage";
+import WebPlayerHome from "./WebPlayerHome";
+import WebPlayerLibrary from "./WebPlayerLibrary";
+import WebPlayerSearch from "./WebPlayerSearch";
+import WebPlayerPlaylist from "./WebPlayerPlaylist";
+
+export {
+  Home,
+  SignUp,
+  Login,
+  ResetPassword,
+  UploadSong,
+  UploadImage,
+  WebPlayerHome,
+  WebPlayerLibrary,
+  WebPlayerSearch,
+  WebPlayerPlaylist,
+};
