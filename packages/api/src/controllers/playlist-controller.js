7200e10e5104d8e20541e191bce365ee070cc690
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-26 01:39:30 +0200

    feat: add update playlist controller impl

--- packages/api/src/controllers/playlist-controller.js
+++ packages/api/src/controllers/playlist-controller.js
@@ -30,6 +30,41 @@ async function createPlaylist(req, res, next) {
   }
 }
 
+async function updatePlaylist(req, res, next) {
+  const {
+    body: { _id: id, name, description, collaborative, thumbnail, tracks },
+  } = req;
+
+  try {
+    let dbResponse = await PlaylistRepo.findById({
+      _id: id,
+    });
+    console.log("FIND RESPONSE");
+    console.log(dbResponse.data);
+
+    if (dbResponse.data) {
+      if (name) dbResponse.data.name = name;
+      if (description) dbResponse.data.description = description;
+      if (collaborative) dbResponse.data.collaborative = collaborative;
+      if (thumbnail) dbResponse.data.thumbnail = thumbnail;
+      if (tracks) {
+        dbResponse.data.tracks = tracks;
+        dbResponse.data.total_tracks = tracks.length;
+      }
+
+      const putResponse = await PlaylistRepo.update(dbResponse.data);
+      dbResponse = putResponse;
+    }
+
+    console.log("UPDATE RESPONSE");
+    console.log(dbResponse.data);
+
+    handleDbResponse(res, dbResponse);
+  } catch (err) {
+    next(err);
+  }
+}
+
 async function fetchPlaylistById(req, res, next) {
   const {
     params: { id },
@@ -60,6 +95,7 @@ async function fetchPlaylists(req, res, next) {
 
 module.exports = {
   createPlaylist: createPlaylist,
+  updatePlaylist: updatePlaylist,
   fetchPlaylists: fetchPlaylists,
   fetchPlaylistById: fetchPlaylistById,
 };
