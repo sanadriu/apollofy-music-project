e650fa6de51623243e51c9946ab56a5fe20ef0d3
Jcarri14 <j.carrion@students.salle.url.edu>
2021-04-27 13:58:38 +0200

    feat: add PieChart to Stats page

--- packages/web/src/pages/WebPlayerStats/WebPlayerStats.js
+++ packages/web/src/pages/WebPlayerStats/WebPlayerStats.js
@@ -12,6 +12,7 @@ import WebPlayerSidebar from "../../components/WebPlayerSidebar/WebPlayerSidebar
 import WebPlayerMainView from "../../components/WebPlayerMainView/WebPlayerMainView";
 
 import BarChart from "../../components/Charts/BarChart";
+import PieChart from "../../components/Charts/PieChart";
 
 function WebPlayerStats() {
   return (
@@ -22,12 +23,36 @@ function WebPlayerStats() {
           <h1>Stats</h1>
           <div className="WebPlayerStats">
             <div className="WebPlayerStats__wrapper">
-              <BarChart
-                title="BarChart test"
-                labels={["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]}
-                values={[12, 19, 3, 5, 2, 3]}
-                valuesMeaning="Usage"
-              />
+              <div className="WebPlayerStats__chart bar">
+                <BarChart
+                  title="BarChart test"
+                  labels={[
+                    "Red",
+                    "Blue",
+                    "Yellow",
+                    "Green",
+                    "Purple",
+                    "Orange",
+                  ]}
+                  values={[12, 19, 3, 5, 2, 3]}
+                  valuesMeaning="Usage"
+                />
+              </div>
+              <div className="WebPlayerStats__chart donut">
+                <PieChart
+                  title="PieChart test"
+                  labels={[
+                    "Red",
+                    "Blue",
+                    "Yellow",
+                    "Green",
+                    "Purple",
+                    "Orange",
+                  ]}
+                  values={[12, 19, 3, 5, 2, 3]}
+                  valuesMeaning="Usage"
+                />
+              </div>
             </div>
           </div>
         </WebPlayerMainView>
