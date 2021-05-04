3288dc072e9d9e93855b916226028eb304c09502
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:18:23 +0200

    feat: update repo indexjs

--- packages/api/src/repositories/index.js
+++ packages/api/src/repositories/index.js
@@ -3,6 +3,12 @@ const TrackRepository = require("./track-repository");
 const PlaylistRepository = require("./playlist-repository");
 const PlaybackRepository = require("./playback-repository");
 const GenreRepository = require("./genre-repository");
+const UserRepository = require("./general/user-repository");
+const TrackRepository = require("./general/track-repository");
+const PlaylistRepository = require("./general/playlist-repository");
+const GenreRepository = require("./general/genre-repository");
+
+const TrackPlaybackRepository = require("./data-analysis/track-playback-repository");
 
 module.exports = {
   UserRepo: UserRepository,
@@ -10,4 +16,5 @@ module.exports = {
   PlaylistRepo: PlaylistRepository,
   PlaybackRepo: PlaybackRepository,
   GenreRepo: GenreRepository,
+  TrackPlaybackRepo: TrackPlaybackRepository,
 };
