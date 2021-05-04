027a3a6e44f1df7b03e7e9b96d5a0ab346a93366
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:23:46 +0200

    feat: update user routes

--- packages/api/src/routes/user-routes.js
+++ packages/api/src/routes/user-routes.js
@@ -1,12 +1,18 @@
 const Router = require("express").Router;
 
-const { authMiddleware } = require("../middlewares");
+const { authMiddleware, geoMiddleware } = require("../middlewares");
 const { userController } = require("../controllers");
 
 const userRouter = Router();
 
-userRouter.post("/sign-up", authMiddleware, userController.signUp);
+userRouter.post(
+  "/sign-up",
+  authMiddleware,
+  geoMiddleware,
+  userController.signUp,
+);
 userRouter.post("/sign-out", authMiddleware, userController.signOut);
+userRouter.get("/users", authMiddleware, userController.fetchUsers);
 
 module.exports = {
   userRouter: userRouter,
