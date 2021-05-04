27057189a8eb5a83e689edfda32297e6b2e7c694
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:08:15 +0200

    feat: update models indexjs

--- packages/api/src/models/index.js
+++ packages/api/src/models/index.js
@@ -1,13 +1,27 @@
-const UserModel = require("./user-model");
-const TrackModel = require("./track-model");
-const PlaylistModel = require("./playlist-model");
-const GenreModel = require("./genre-model");
-const PlaybackModel = require("./playback-model");
+// General
+const UserModel = require("./general/user-model");
+const TrackModel = require("./general/track-model");
+const PlaylistModel = require("./general/playlist-model");
+const GenreModel = require("./general/genre-model");
+// Data Analysis
+const DailyPageRequestModel = require("./data-analysis/daily-page-request-model");
+const MonthlyPageRequestModel = require("./data-analysis/monthly-page-request-model");
+
+const TrackPlaybackModel = require("./data-analysis/track-playback-model");
+const MonthlyTrackPlaybackModel = require("./data-analysis/monthly-track-playback-model");
+
+const DailyGenrePlaybackModel = require("./data-analysis/daily-genre-playback-model");
+const MonthlyGenrePlaybackModel = require("./data-analysis/monthly-genre-playback-model");
 
 module.exports = {
   User: UserModel,
   Track: TrackModel,
   Playlist: PlaylistModel,
   Genre: GenreModel,
-  Playback: PlaybackModel,
+  DailyPageRequest: DailyPageRequestModel,
+  MonthlyPageRequest: MonthlyPageRequestModel,
+  TrackPlayback: TrackPlaybackModel,
+  MonthlyTrackPlayback: MonthlyTrackPlaybackModel,
+  DailyGenrePlayback: DailyGenrePlaybackModel,
+  MonthlyGenrePlayback: MonthlyGenrePlaybackModel,
 };
