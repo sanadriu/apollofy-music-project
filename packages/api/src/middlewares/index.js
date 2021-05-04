c5fc8fa90e24a880e33bd93a38a6579429882d9e
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:19:48 +0200

    feat: add geo middleware reference in indexjs

--- packages/api/src/middlewares/index.js
+++ packages/api/src/middlewares/index.js
@@ -1,7 +1,9 @@
 const { authMiddleware } = require("./auth-middleware");
 const { errorMiddleware } = require("./error-middleware");
+const { geoMiddleware } = require("./geo-middleware");
 
 module.exports = {
   authMiddleware: authMiddleware,
   errorMiddleware: errorMiddleware,
+  geoMiddleware: geoMiddleware,
 };
