1322210e16de98c94c3d1bc929c60eea4ce32951
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:21:14 +0200

    feat: add track playback controller reference

--- packages/api/src/controllers/index.js
+++ packages/api/src/controllers/index.js
@@ -3,9 +3,11 @@ const trackController = require("./track-controller");
 const playlistController = require("./playlist-controller");
 const genreController = require("./genre-controller");
 
+const trackPlaybackController = require("./track-playback-controller");
 module.exports = {
   userController: userController,
   trackController: trackController,
   playlistController: playlistController,
   genreController: genreController,
+  trackPlaybackController: trackPlaybackController,
 };
