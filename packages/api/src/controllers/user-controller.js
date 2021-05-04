c06547a542cbb0ae002c80dfbf739646f2e66958
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:20:23 +0200

    feat: add fetchUsers handler function

--- packages/api/src/controllers/user-controller.js
+++ packages/api/src/controllers/user-controller.js
@@ -43,7 +43,28 @@ async function signOut(req, res) {
   });
 }
 
+async function fetchUsers(req, res, next) {
+  try {
+    const dbResponse = await UserRepo.find();
+
+    if (dbResponse.error) {
+      res.status(400).send({
+        data: null,
+        error: dbResponse.error,
+      });
+    }
+
+    res.status(200).send({
+      data: dbResponse.data,
+      error: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   signUp: signUp,
   signOut: signOut,
+  fetchUsers: fetchUsers,
 };
