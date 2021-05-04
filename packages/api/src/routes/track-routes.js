05cd60f84258c5d0ceee6d17e06c514e182f88d6
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:23:23 +0200

    feat: update track routes

--- packages/api/src/routes/track-routes.js
+++ packages/api/src/routes/track-routes.js
@@ -1,13 +1,18 @@
 const Router = require("express").Router;
 
 const { authMiddleware } = require("../middlewares");
-const { trackController } = require("../controllers");
+const { trackController, trackPlaybackController } = require("../controllers");
 
 const trackRouter = Router();
 
 trackRouter.post("/tracks", authMiddleware, trackController.createTrack);
 trackRouter.get("/tracks", authMiddleware, trackController.fetchTracks);
 trackRouter.get("/tracks/:id", authMiddleware, trackController.fetchTrackById);
+trackRouter.post(
+  "/tracks/:id/playback",
+  authMiddleware,
+  trackPlaybackController.addPlayback,
+);
 
 module.exports = {
   trackRouter: trackRouter,
