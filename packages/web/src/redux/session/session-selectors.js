1204c09e9007309fe2f67ff16a8cb12b8528547a
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-04 10:43:19 +0200

    feat: add user agent and coordinates selectors

--- packages/web/src/redux/session/session-selectors.js
+++ packages/web/src/redux/session/session-selectors.js
@@ -1,19 +1,5 @@
-import { createSelector } from "reselect";
+export const selectUserAgent = (state) => state.session.userAgent;
 
-export const selectToken = (state) => state.session.currentUser.token;
-
-export const selectCurrentUser = (state) => state.session.currentUser;
+export const selectCoordinates = (state) => state.session.coordinates;
 
 export const selectSessionState = (state) => state.session;
-
-export const tokenSelector = createSelector([selectToken], (token) => token);
-
-export const currentUserSelector = createSelector(
-  [selectCurrentUser],
-  (currentUser) => currentUser,
-);
-
-export const sessionStateSelector = createSelector(
-  [selectSessionState],
-  (sessionState) => sessionState,
-);
