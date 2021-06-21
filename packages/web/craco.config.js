9d81859102b41eaf7ddc9211ba94f9f0f7eda8a7
Jcarri14 <j.carrion@students.salle.url.edu>
2021-06-21 09:17:45 +0200

    fix: craco conf for netlify deployment

--- packages/web/craco.config.js
+++ packages/web/craco.config.js
@@ -3,7 +3,9 @@ const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const postcssFocusVisible = require("postcss-focus-visible");
 
-const purgecss = require("@fullhuman/postcss-purgecss")({
+const purgecss = require("@fullhuman/postcss-purgecss");
+// eslint-disable-next-line spaced-comment
+/*const purgecss = require("@fullhuman/postcss-purgecss")({
   content: [
     path.resolve(__dirname, "..", "src", "**", "!(*.test).js"),
     path.resolve(__dirname, "..", "src", "**", "*.html"),
@@ -11,8 +13,23 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // This extractor is used for tailwind classes.
   // Read more here: https://tailwindcss.com/docs/controlling-file-size/
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
+});*/
 
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        postcssFocusVisible,
+        autoprefixer,
+        purgecss({
+          content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.ts"],
+        }),
+      ],
+    },
+  },
+};
+/*
 module.exports = {
   style: {
     postcss: {
@@ -25,3 +42,4 @@ module.exports = {
     },
   },
 };
+*/
